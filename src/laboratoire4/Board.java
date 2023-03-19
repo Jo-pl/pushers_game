08aa9d90f2c0837918be5ce07d70a08e5d230c9b
ggr <a@a.a>
2023-03-19 17:50:28 -0400

    Network + play

--- src/laboratoire4/Board.java
+++ src/laboratoire4/Board.java
@@ -5,30 +5,41 @@ import java.util.HashMap;
 
 public class Board {
 
-    enum Case {
-        Vide,
-        Noir,
-        NoirPusher,
-        Rouge,
-        RougePusher
+    public enum Case {
+        Vide(0),
+        Noir(1),
+        NoirPusher(2),
+        Rouge(3),
+        RougePusher(4);
+
+        private int v;
+        private Case(int v) {
+            this.v = v;
+        }
+        public int value() {
+            return v;
+        }
     }
 
     private final Case[][] board = new Case[8][8];
     private final boolean isSecondPlayerTurn;
 
-    // Caches of the boards to save some computation time
-    private HashMap<Move, Board> nextMoves = new HashMap<Move, Board>();
-
     public Board(String init, boolean isSecondPlayerTurn) {
         this.isSecondPlayerTurn = isSecondPlayerTurn;
-        // TODO - Init the board
+        String[] s = init.split(" ");
+        if (s.length != 64) {
+            // ???
+        }
+        for (int i = 0; i < s.length; i++) {
+            board[i / 8][i % 8] = Case.values()[Integer.parseInt(s[i])];
+        }
     }
 
     // KEEP PRIVATE - use Board.getNextMove(Move) instead
     private Board(Board parent, Move move) {
         this.isSecondPlayerTurn = !parent.isSecondPlayerTurn;
-        for (int x = 0; x < 7; x++) {
-            for (int y = 0; y < 7; y++) {
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
                 if (x == move.colA && y == move.rowA) {
                     board[y][x] = Case.Vide;
                 } else if (x == move.colB && y == move.rowB) {
@@ -41,21 +52,25 @@ public class Board {
     }
 
     // Get all possible next moves
+    private ArrayList<Move> cachedMoves;
     public ArrayList<Move> getMoves() {
-        ArrayList<Move> moves = new ArrayList<Move>();
+        if (cachedMoves == null) {
+            cachedMoves = new ArrayList<Move>();
 
-        // TODO: Make a list of possible moves
-
-        return moves;
-    }
-
-    // Get the board corresponding to a certain move. The move is NOT validated!
-    public Board getNextMove(Move move) {
-        if (!nextMoves.containsKey(move)) {
-            nextMoves.put(move, new Board(this, move));
+            // TODO: Make a list of possible moves
         }
 
-        return nextMoves.get(move);
+        return cachedMoves;
+    }
+
+    // Get the board corresponding to a certain move. The move is NOT verified to be valid!
+    private HashMap<Move, Board> cachedNextMoves = new HashMap<Move, Board>();
+    public Board getNextMove(Move move) {
+        if (!cachedNextMoves.containsKey(move)) {
+            cachedNextMoves.put(move, new Board(this, move));
+        }
+
+        return cachedNextMoves.get(move);
     }
 
     private boolean hasCachedHeuristic = false;
@@ -70,6 +85,18 @@ public class Board {
         return cachedHeuristic;
     }
 
+    @Override
+    public String toString() {
+        String s = new String();
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                s += board[y][x].value() + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
     public static class Move {
         public final int colA, rowA, colB, rowB;
 
@@ -81,15 +108,20 @@ public class Board {
         }
 
         public Move(String cmd) {
+            cmd = cmd.trim();
             this.colA = cmd.charAt(0) - 'A';
             this.rowA = cmd.charAt(1) - '1';
-            this.colB = cmd.charAt(3) - 'A';
-            this.rowB = cmd.charAt(4) - '1';
+            this.colB = cmd.charAt(5) - 'A';
+            this.rowB = cmd.charAt(6) - '1';
+        }
+
+        public static boolean isValid(String cmd) {
+            return cmd != null && cmd.matches("^[A-H][1-8] - [A-H][1-8]$");
         }
 
         @Override
         public String toString() {
-            return new String(new byte[] { (byte) ('A' + colA) }) + (rowA + 1) + "-"
+            return new String(new byte[] { (byte) ('A' + colA) }) + (rowA + 1) + " - "
                  + new String(new byte[] { (byte) ('A' + colB) }) + (rowB + 1);
         }
     }
--- src/laboratoire4/Client4.java
+++ src/laboratoire4/Client4.java
@@ -11,114 +11,106 @@ import java.util.Hashtable;
 
 public class Client4 {
 
-    /*Might be useful someday
-    enum tokenType {
-        Case_vide,
-        Petit_pion_noir,
-        Pion_noir,
-        Petit_pion_rouge,
-        Pion_rouge,
-    }
-    */
-    //Color red = 0; black = 1;
-    private int color = 0;
-    private int[] pieceCode = {1,2};
-    private Hashtable<Character,Integer> columnTableAssociation = new Hashtable<Character,Integer>();
-    private int[][] board = new int[8][8];
-
     public static void main(String[] args) throws IOException {
-        Socket MyClient;
-        try {
-            MyClient = new Socket("localhost", 8888);
-            BufferedInputStream input = new BufferedInputStream(MyClient.getInputStream());
-            BufferedOutputStream output = new BufferedOutputStream(MyClient.getOutputStream());
+        try (Socket s = new Socket("localhost", 8888)) {
+            BufferedInputStream input = new BufferedInputStream(s.getInputStream());
+            BufferedOutputStream output = new BufferedOutputStream(s.getOutputStream());
             BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Connected");
             Client4 client = new Client4();
-            client.gameOn(input, output, console);
+            client.play(input, output, console);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void gameOn(BufferedInputStream input, BufferedOutputStream output, BufferedReader console) throws IOException {
+    public void play(BufferedInputStream input, BufferedOutputStream output, BufferedReader console) throws IOException {
 
         byte[] aBuffer = new byte[1024];
         int size = 0;
         String s = null;
+        String move = null;
+        Board board = null, lastBoard = null;
 
         while (true) {
             char cmd = 0;
             cmd = (char) input.read();
-            // System.out.println(cmd);
+
             switch (cmd) {
-                case '1': // You have the first move
-                    this.color = 0;
-                    this.pieceCode[0] = 3;
-                    this.pieceCode[1] = 4;
+                case '1': // New game (I'm player 1)
+                case '2': // New game (I'm player 2)
                     aBuffer = new byte[1024];
                     size = input.available();
-                    // System.out.println("size " + size);
                     input.read(aBuffer, 0, size);
                     s = new String(aBuffer).trim();
-                    System.out.println(s);
-                    buildTable(s);
-                    viewTable(this.board);
-                    System.out.println("Nouvelle partie! Vous jouer blanc, entrez votre premier coup : ");                    
-                    String move = null;
-                    move = console.readLine();
-                    updateTable(move);
-                    output.write(move.getBytes(), 0, move.length());
-                    output.flush();
+
+                    board = new Board(s, cmd == 1);
+
+                    System.out.println(board);
+                    if (cmd == '1') {
+                        System.out.println("Nouvelle partie, joueur " + cmd + " (à vous!) :");
+                        do {
+                            move = console.readLine();
+                        } while (!Board.Move.isValid(move));
+
+                        lastBoard = board;
+                        board = board.getNextMove(new Board.Move(move));
+
+                        output.write(move.getBytes(), 0, move.length());
+                        output.flush();
+                    }
                     break;
-                case '2': // You have the second move
-                    this.color = 1;
-                    aBuffer = new byte[1024];
-                    size = input.available();
-                    // System.out.println("size " + size);
-                    input.read(aBuffer, 0, size);
-                    s = new String(aBuffer).trim();
-                    System.out.println(s);
-                    buildTable(s);
-                    viewTable(this.board);
-                    break;
+
                 case '3': // New move
                     aBuffer = new byte[16];
                     size = input.available();
-                    System.out.println("size :" + size);
                     input.read(aBuffer, 0, size);
                     s = new String(aBuffer);
-                    System.out.println("Dernier coup :" + s);
-                    updateTable(s);
-                    Node node = buildTree();
-                    System.out.println("Entrez votre coup : ");
-                    //Implementation of our giveNextMove function
-                    move = null;
-                    move = console.readLine();
-                    updateTable(move);
-                    node = buildTree();
-                    output.write(move.getBytes(), 0, move.length());
-                    output.flush();
-                System.out.println("end of cmd3");
-                    break;
-                case '4': //Move is invalid
-                    System.out.println("Coup invalide, entrez un nouveau coup : ");
-                    move = null;
-                    move = console.readLine();
+
+                    lastBoard = board;
+                    board = board.getNextMove(new Board.Move(s));
+
+                    System.out.println("Dernier coup : " + s);
+                    System.out.println(board);
+                    System.out.println("Votre coup :");
+
+                    do {
+                        move = console.readLine();
+                    } while (!Board.Move.isValid(move));
+
+                    lastBoard = board;
+                    board = board.getNextMove(new Board.Move(move));
+
                     output.write(move.getBytes(), 0, move.length());
                     output.flush();
                     break;
-                case '5': //Game over
+
+                case '4': // Move is invalid
+                    System.out.println("Coup invalide, veuillez entrer votre coup :");
+
+                    board = lastBoard;
+                    do {
+                        move = console.readLine();
+                    } while (!Board.Move.isValid(move));
+
+                    lastBoard = board;
+                    board = board.getNextMove(new Board.Move(move));
+
+                    output.write(move.getBytes(), 0, move.length());
+                    output.flush();
+                    break;
+
+                case '5': // Game over
                     aBuffer = new byte[16];
                     size = input.available();
                     input.read(aBuffer, 0, size);
                     s = new String(aBuffer);
+
                     System.out.println("Partie Terminé. Le dernier coup joué est: " + s);
-                    move = null;
-                    move = console.readLine();
-                    output.write(move.getBytes(), 0, move.length());
-                    output.flush();
+                    lastBoard = null;
+                    board = null;
                     break;
+
                 default:
                     // The input cmd also print spaces
                     break;
@@ -126,116 +118,4 @@ public class Client4 {
         }
 
     }
-
-    public void initializeColumnTableAssociation(){
-        this.columnTableAssociation.put('A',1);
-        this.columnTableAssociation.put('B',2);
-        this.columnTableAssociation.put('C',3);
-        this.columnTableAssociation.put('D',4);
-        this.columnTableAssociation.put('E',5);
-        this.columnTableAssociation.put('F',6);
-        this.columnTableAssociation.put('G',7);
-        this.columnTableAssociation.put('H',8);
-    }
-
-    public void viewTable(int[][] board) {
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                System.out.print(board[j][i]);
-            }
-            System.out.println();
-        }
-    }
-
-    public void buildTable(String s) {
-        System.out.println(s);
-        String[] boardValues;
-        boardValues = s.split(" ");
-        int x = 0, y = 0;
-        for (int i = 0; i < boardValues.length; i++) {
-            board[x][y] = Integer.parseInt(boardValues[i]);
-            x++;
-            if (x == 8) {
-                x = 0;
-                y++;
-            }
-        }
-    }
-
-    public void updateTable(String s) {
-        char[] currentMove = s.replaceAll("\\s+","").toCharArray();
-        //New token
-        this.board[this.columnTableAssociation.get(currentMove[3])-1][this.board[0].length-Character.getNumericValue(currentMove[4])] = 
-            this.board[this.columnTableAssociation.get(currentMove[0])-1][this.board[0].length-Character.getNumericValue(currentMove[1])];
-        //Delete the old token
-        this.board[this.columnTableAssociation.get(currentMove[0])-1][this.board[0].length-Character.getNumericValue(currentMove[1])] = 0;
-        viewTable(this.board);
-    }
-
-    public Node buildTree() {
-        /*
-        TODO edit method to account for black pieces movement by either 
-        flipping the board or using variables to add or remove from j and i positions instead of constants
-        */
-        Node node = new Node();
-        node.setBoard(this.board);
-        // for every space there is on the board
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                // is a small piece
-                if (board[j][i] == pieceCode[0]) {
-                    // Middle move
-                    //checks if space in front isn't occupied
-                    if (board[j][i - 1] == 0 && board[j][i + 1] == pieceCode[1]) {
-                        addMove(node, i, j, i - 1, j);
-                    }
-                    //Left and right moves
-                    //same here for sides
-                    if (j != 0 && j != 7) {                        
-                        if (board[j + 1][i + 1] == pieceCode[1]
-                                && (board[j - 1][i - 1] != pieceCode[0] && board[j - 1][i - 1] != pieceCode[1])) {
-                            addMove(node, i, j, i - 1, j - 1);
-                        }
-                        if (board[j - 1][i + 1] == pieceCode[1]
-                                && (board[j + 1][i - 1] != pieceCode[0] && board[j + 1][i - 1] != pieceCode[1])) {
-                            addMove(node, i, j, i - 1, j + 1);
-                        }
-                    }
-                    // is a big piece
-                } else if (board[j][i] == pieceCode[1]) {
-                    //checks if space in front isn't occupied
-                    if (board[j][i - 1] != pieceCode[0] && board[j][i - 1] != pieceCode[1]) {
-                        if (board[j][i - 1] == 0) {
-                            addMove(node, i, j, i - 1, j);
-                        }
-                        //similar to previous
-                        if(j!=0 && (board[j - 1][i - 1] != pieceCode[0] && board[j - 1][i - 1] != pieceCode[1])){
-                            addMove(node, i, j, i - 1, j-1);
-                        }
-                        if(j!=7 && (board[j + 1][i - 1] != pieceCode[0] && board[j + 1][i - 1] != pieceCode[1])){
-                            addMove(node, i, j, i - 1, j+1);
-                        }                        
-                    }
-                }
-                // Color black or empty space
-            }
-        }
-        //TODO remove in production
-        int i = 1;
-        for (Node child : node.getChildren()) {
-            System.out.println("Board " + i);
-            viewTable(child.getBoard());
-            i++;
-        }
-        return node;
-    }
-
-    public void addMove(Node node, int i, int j,int tempi,int tempj){
-        int[][] tempBoard = Arrays.stream(this.board).map(int[]::clone).toArray(int[][]::new);//Reference: https://stackoverflow.com/questions/5617016/how-do-i-copy-a-2-dimensional-array-in-java
-        tempBoard[tempj][tempi] = tempBoard[j][i];
-        tempBoard[j][i] = 0;
-        Node tempNode = new Node();
-        tempNode.setBoard(tempBoard);
-        node.addChildren(tempNode);
-    }    
 }
